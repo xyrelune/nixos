d7fd829d0f53fbfc4ced5639717c1b14dab54498
kin <kinest@tutamail.com>
2026-07-24 10:36:18 +0530

    commit

---
+++ modules/features/web/rclone.nix
@@ -0,0 +1,37 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.rclone = {
+    pkgs,
+    config,
+    ...
+  }: {
+    sops.secrets = {
+      "rclone/tiredlamp/token/access_token" = {};
+      "rclone/tiredlamp/token/refresh_token" = {};
+      "rclone/tiredlamp/client_id" = {};
+      "rclone/tiredlamp/client_secret" = {};
+    };
+    sops.templates."rclone.conf".content = ''
+      [tiredlamp_gdrive]
+      type = drive
+      client_id = ${config.sops.secrets."rclone/tiredlamp/client_id"}
+      client_secret = ${config.sops.secrets."rclone/tiredlamp/client_secret"}
+      scope = drive
+      token = {"access_token":"${config.sops.secrets."rclone/tiredlamp/token/access_token"}","token_type":"Bearer","refresh_token":"${config.sops.secrets."rclone/tiredlamp/token/refresh_token"}","expiry":"2026-07-22T09:30:31.915272781+05:30","expires_in":3599}
+      teamdrive = 
+    '';
+    environment.systemPackages = [ pkgs.rclone ];
+    fileSystems."/mnt/tiredlamp" = {
+      device = "tiredlamp_gdrive:/bitwarden";
+      fsType = "rclone";
+      options = [
+        "nodev"
+        "nofail"
+        "allow_other"
+        "args2env"
+        "config=${config.sops.templates."rclone.conf".path}"
+      ];
+    };
--- modules/hosts/server/configuraion.nix
+++ modules/hosts/server/configuraion.nix
@@ -33,6 +33,7 @@
       self.nixosModules.arr
       self.nixosModules.jellyfin
       self.nixosModules.copyparty
+      self.nixosModules.rclone
 
       inputs.sops-nix.nixosModules.sops
     ];
