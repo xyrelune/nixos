faff48e3414cb536b3f4d413f93d2fe03be41d0d
kin <kinest@tutamail.com>
2026-08-15 15:23:16 +0530

    commit

--- modules/features/misc/docker-compose.nix
+++ modules/features/misc/docker-compose.nix
@@ -24,13 +24,13 @@
     };
 
     # Enable container name DNS for all Podman networks.
-    networking.firewall.interfaces = let
-        matchAll = if !config.networking.nftables.enable then "podman+" else "podman*";
-    in {
-        "${matchAll}".allowedUDPPorts = [ 53 ];
-    };
+    #networking.firewall.interfaces = let
+    #    matchAll = if !config.networking.nftables.enable then "podman+" else "podman*";
+    #in {
+    #    "${matchAll}".allowedUDPPorts = [ 53 ];
+    #};
 
-    virtualisation.oci-containers.backend = "podman";
+    #virtualisation.oci-containers.backend = "podman";
 
     # Containers
     virtualisation.oci-containers.containers."multi-scrobbler" = {
