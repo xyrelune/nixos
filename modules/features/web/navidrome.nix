327db066e8d4fdd8d98b066ad348a3c3a1bb3dc2
kin <kinest@tutamail.com>
2026-08-27 13:36:59 +0530

    commit

--- modules/features/web/navidrome.nix
+++ modules/features/web/navidrome.nix
@@ -6,6 +6,8 @@
   flake.nixosModules.navidrome = {
     services.navidrome = {
       enable = true;
+      user = "media";
+      group = "media";
       settings = {
         MusicFolder = "/home/kin/personal-music";
       };
