93708517b101b5a6156fe053e4fd391b687604d6
kin <kinest@tutamail.com>
2026-09-26 03:24:23 +0530

    commit

--- flake.nix
+++ flake.nix
@@ -51,9 +51,5 @@
       url = "github:wimpysworld/sidra";
     };
   };
-  nixConfig = {
-    extra-substituters = [ "https://cache.nixos-cuda.org" ];
-    extra-trusted-public-keys = [ "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M=" ];
-  };
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
 }
